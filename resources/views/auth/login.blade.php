e8ef39f0d9dcf8e68544dbd011e214f53538f586
juankrlos1521 <carlos.92_1@hotmail.com>
2024-04-15 22:32:17 -0500

    login_arreglado
    login basico administrador

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,7 +17,7 @@
 
             <div>
                 <x-label for="username" value="{{ __('Username') }}" />
-                <x-input id="username" class="block mt-1 w-full" type="email" name="username" :value="old('username')" required autofocus autocomplete="username" />
+                <x-input id="username" class="block mt-1 w-full" type="text" name="username" :value="old('username')" required autofocus autocomplete="username" />
             </div>
 
             <div class="mt-4">
